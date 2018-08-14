4849802d9cd28e204e34cd826e0f61aced64e7a3
sipocz <30761411+sipocz@users.noreply.github.com>
2018-08-14 14:26:27 +0200

    cosmetic change, comments

--- ZEnemy/h-stats.sh
+++ ZEnemy/h-stats.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 API_PORT=4068
-API_TIMEOUT=5
+API_TIMEOUT=5 # need more test on it.
+# please update the list of algos
 algo_avail=("lyra2z" "tribus" "phi" "phi2" "c11" "x17" "x16r" "x16s" "renesis" "hex")
 
 #######################
@@ -18,17 +19,18 @@ get_cards_hashes(){
 	
 	local gpu_total=$GPU_COUNT_NVIDIA
 	#echo "GPU total:" $gpu_total
-	for (( i=0; i < gpu_total; i++ )); do
+for (( i=0; i < gpu_total; i++ )); do
 	
-	index=$((i+1))
+index=$((i+1))
 	
-	case $index in
+case $index in
 1)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $1}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $1}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 
 ;;
+
 2)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $2}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $2}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
@@ -42,12 +44,14 @@ local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $3}'  | awk -F 'KHS=' '{
 
 
 ;;
+
 4)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $4}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $4}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 
 ;;
+
 5)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $5}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $5}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
@@ -67,36 +71,43 @@ local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $7}' | awk -F 'GPU=' '{pri
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $7}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 8)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $8}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $8}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 9)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $9}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $9}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 10)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $10}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $10}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 11)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $11}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $11}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 12)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $12}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $12}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 13)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $13}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $13}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 14)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $14}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $14}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
@@ -108,6 +119,7 @@ local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $15}' | awk -F 'GPU=' '{pr
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $15}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 16)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $16}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $16}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
@@ -119,6 +131,7 @@ local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $17}' | awk -F 'GPU=' '{pr
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $17}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 18)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $18}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $18}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
@@ -130,33 +143,12 @@ local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $19}' | awk -F 'GPU=' '{pr
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $19}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
+
 20)
 local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $20}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
 local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $20}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
 
 ;;
-
-14)
-local gpu_id=$(echo $miner_stat2 | awk -F '|' '{print $14}' | awk -F 'GPU=' '{print $2 }' | awk -F ';|=' '{print $1}' )
-local hashrate=$(echo $miner_stat2 | awk -F '|' '{print $14}'  | awk -F 'KHS=' '{print $2 }' | awk -F ';|=' '{print $1}')
-
-;;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 *)
 
@@ -165,10 +157,10 @@ echo "caser" $index
 
 
 esac
-	#echo "i:" $i "GPUID: " $gpu_id
+#echo "i:" $i "GPUID: " $gpu_id
 	
-	hs[$gpu_id]=$hashrate
-	done
+hs[$gpu_id]=$hashrate
+done
 }
 
 get_nvidia_cards_temp(){
@@ -237,7 +229,7 @@ fi
 	get_cards_hashes					# hashes array
 	hs_units='khs'						# hashes utits
 	temp=$(get_nvidia_cards_temp)		# cards temp
-	fan=$(get_nvidia_cards_fan)			# cards fan
+	fan=$(get_nvidia_cards_fan)		# cards fan
 echo "Fans:" $fan
 echo "Temperatures: " $temp
 
@@ -255,7 +247,7 @@ echo -e "${RED}Rejected Shares:${NOCOLOR}" $rj
 
 
 
-#	# make JSON
+## make JSON
 	stats=$(jq -nc \
 				--argjson hs "`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`" \
 				--arg hs_units "$hs_units" \
@@ -266,9 +258,9 @@ echo -e "${RED}Rejected Shares:${NOCOLOR}" $rj
 				--arg algo "$algo" \
 				'{$hs, $hs_units, $temp, $fan, $uptime, ar: [$ac, $rj], $algo}')
 
-	# total hashrate in khs
+# total hashrate in khs
 
-	khs=$(get_total_hashes)
+khs=$(get_total_hashes)
 
 # debug output
 echo temp:   $temp
